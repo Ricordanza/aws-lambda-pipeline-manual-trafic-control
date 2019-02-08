f7fac17fbdc94f87307df812f273ed32fa4f8e26
Toshiaki Aoike <aoike@cloudpack.jp>
2019-02-08 16:49:06 +0900

    first commit

---
+++ cfn/_remove.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+pushd `dirname $0`
+
+# variable
+source ./_variable.sh
+
+echo "delete bucket"
+aws s3 rm s3://$SYS_NAME --recursive
+
+echo "delete code repository"
+aws codecommit delete-repository --repository-name $SYS_NAME
+
+echo "delete stack"
+aws cloudformation delete-stack --stack-name $SYS_NAME
+echo "stack deleting ..."
+aws cloudformation wait stack-delete-complete --stack-name $SYS_NAME
+
+echo "remove finished !"
+
+exit 0
